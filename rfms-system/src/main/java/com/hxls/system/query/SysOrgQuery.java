a636824d0fd065fcd3bebcb68dab3815607c975a
zhwei0601 <1991247026@qq.com>
2024-03-27 17:14:08 +0800

    feat: 新增组织查询类

---
+++ rfms-system/src/main/java/com/hxls/system/query/SysOrgQuery.java
@@ -0,0 +1,30 @@
+package com.hxls.system.query;
+
+import com.hxls.framework.common.query.Query;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 组织查询
+ *
+ * @author
+ *
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Schema(description = "组织查询")
+public class SysOrgQuery extends Query {
+    @Schema(description = "组织编码")
+    private String code;
+
+    @Schema(description = "组织名称")
+    private String name;
+
+    @Schema(description = "上级组织编码")
+    private String pcode;
+
+    @Schema(description = "类型")
+    private Integer property;
+
+}
