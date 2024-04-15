797af975ad501c59cc3a8c5896dad42bbbfb83cb
zhwei0601 <1991247026@qq.com>
2024-04-15 21:44:44 +0800

    feat: 新增主数据岗位数据VO

---
+++ rfms-system/src/main/java/com/hxls/system/vo/MainPostVO.java
@@ -0,0 +1,21 @@
+package com.hxls.system.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * @author zhaohong
+ * @version 1.0
+ * @class_name MainPostVO
+ * @create_date 2024/4/15 17:20
+ * @description 主数据岗位数据VO
+ */
+@Data
+public class MainPostVO {
+
+    @Schema(description = "id")
+    private String id;
+
+    @Schema(description = "岗位名字")
+    private String postName;
+}
