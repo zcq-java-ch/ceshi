b46892b28088fe27897e35466101b18ff7cef5fb
admin <1340774602@qq.com>
2024-04-19 18:10:55 +0800

    feat: 查询派驻人员数量和所有id

--- rfms-appointment/src/main/java/com/hxls/appointment/controller/api/AppointmentApiController.java
+++ rfms-appointment/src/main/java/com/hxls/appointment/controller/api/AppointmentApiController.java
@@ -1,10 +1,15 @@
 package com.hxls.appointment.controller.api;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.json.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.hxls.api.dto.appointment.AppointmentDTO;
+import com.hxls.appointment.pojo.entity.TAppointmentEntity;
+import com.hxls.appointment.pojo.entity.TAppointmentPersonnel;
 import com.hxls.appointment.pojo.query.TAppointmentQuery;
 import com.hxls.appointment.pojo.vo.TAppointmentVO;
 import com.hxls.appointment.server.RabbitMqManager;
+import com.hxls.appointment.service.TAppointmentPersonnelService;
 import com.hxls.appointment.service.TAppointmentService;
 import com.hxls.framework.common.cache.RedisCache;
 import com.hxls.framework.common.utils.PageResult;
@@ -15,8 +20,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.apache.xmlbeans.impl.xb.xsdschema.All;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/appointment")
@@ -28,6 +39,7 @@ public class AppointmentApiController {
     private final RabbitMqManager rabbitMqManager;
     private final RedisCache redisCache;
     private final TAppointmentService tAppointmentService;
+    private final TAppointmentPersonnelService tAppointmentPersonnelService;
 
 
     @PostMapping("establish")
@@ -115,6 +127,47 @@ public class AppointmentApiController {
         }
 
         return true;
+    }
+
+    @PostMapping("queryTheNumberOfResidencies")
+    @Operation(summary = "查询派驻人员数量和所有id")
+    public JSONObject queryTheNumberOfResidencies(@RequestParam Long siteId) {
+
+        Integer pzNum = 0;
+        LambdaQueryWrapper<TAppointmentEntity> tAppointmentEntityLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        tAppointmentEntityLambdaQueryWrapper.eq(TAppointmentEntity::getStatus, 1);
+        tAppointmentEntityLambdaQueryWrapper.eq(TAppointmentEntity::getDeleted, 0);
+        tAppointmentEntityLambdaQueryWrapper.eq(TAppointmentEntity::getSiteId, siteId);
+        tAppointmentEntityLambdaQueryWrapper.eq(TAppointmentEntity::getReviewStatus, 1); // 审核通过
+        List<TAppointmentEntity> tAppointmentEntities = tAppointmentService.list(tAppointmentEntityLambdaQueryWrapper);
+        List<Long> wpPersonnel = new ArrayList<>();
+        List<TAppointmentPersonnel> AllpersonnelList = new ArrayList<>();
+        if (CollectionUtil.isNotEmpty(tAppointmentEntities)){
+            for (int i = 0; i < tAppointmentEntities.size(); i++) {
+                TAppointmentEntity tAppointmentEntity = tAppointmentEntities.get(i);
+                List<TAppointmentPersonnel> personnelList = tAppointmentPersonnelService.list(new LambdaQueryWrapper<TAppointmentPersonnel>().eq(TAppointmentPersonnel::getAppointmentId, tAppointmentEntity.getId()));
+                pzNum += personnelList.size();
+                List<Long> collect = personnelList.stream().map(TAppointmentPersonnel::getId).collect(Collectors.toList());
+                wpPersonnel.addAll(collect);
+
+                AllpersonnelList.addAll(personnelList);
+            }
+        }
+
+        // 按照 busis 字段进行分组
+        Map<String, Long> postCounts = AllpersonnelList.stream()
+                .collect(Collectors.groupingBy(TAppointmentPersonnel::getPostCode, Collectors.counting()));
+        // 打印每个类型及其数量
+        JSONObject postobjects = new JSONObject();
+        for (Map.Entry<String, Long> entry2 : postCounts.entrySet()) {
+            System.out.println("岗位：" + entry2.getKey() + "，数量：" + entry2.getValue());
+            postobjects.putOnce(entry2.getKey(), entry2.getValue());
+        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putOnce("pzNum", pzNum);
+        jsonObject.putOnce("pzAllIds", wpPersonnel);
+        jsonObject.putOnce("postAll", postobjects);
+        return jsonObject;
     }
 
 }
