b3b1511a2eafed1ca5a3a9c114829af5a3053329
zhuchuanqiu <tao.sun@landmark-phb.com>
2024-05-07 15:38:49 +0800

    feat:(记录模块)新增小程序扫码入场出场接口（免权）

---
+++ rfms-datasection/src/main/java/com/hxls/datasection/controller/api/TPersonAccessController.java
@@ -0,0 +1,42 @@
+package com.hxls.datasection.controller.api;
+
+import com.hxls.datasection.service.TPersonAccessRecordsService;
+import com.hxls.datasection.vo.TPersonAccessRecordsVO;
+import com.hxls.framework.common.utils.Result;
+import com.hxls.framework.operatelog.annotations.OperateLog;
+import com.hxls.framework.operatelog.enums.OperateTypeEnum;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/tPersonAccess")
+@Tag(name="小程序人员扫码")
+@AllArgsConstructor
+@Slf4j
+public class TPersonAccessController {
+
+    private final TPersonAccessRecordsService tPersonAccessRecordsService;
+
+
+    @PostMapping("/saveTpersonAccessRecords")
+    @Operation(summary = "保存")
+    @OperateLog(type = OperateTypeEnum.INSERT)
+    public Result<String> saveTpersonAccessRecords(@RequestBody TPersonAccessRecordsVO vo){
+        try {
+            tPersonAccessRecordsService.save(vo);
+            return Result.ok();
+        }catch (Exception e){
+            e.printStackTrace();
+            log.info("保存失败，进入catch:");
+            return Result.error("添加失败！");
+        }
+    }
+
+}
