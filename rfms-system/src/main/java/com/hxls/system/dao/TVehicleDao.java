760755a2ac59a778f55d74897804836b2c307d9d
zhwei0601 <1991247026@qq.com>
2024-05-11 15:24:28 +0800

    fix: 添加车辆，修改车辆，批量导入车辆的时候做车牌号唯一判断

--- rfms-system/src/main/java/com/hxls/system/dao/TVehicleDao.java
+++ rfms-system/src/main/java/com/hxls/system/dao/TVehicleDao.java
@@ -1,5 +1,6 @@
 package com.hxls.system.dao;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hxls.framework.mybatis.dao.BaseDao;
 import com.hxls.system.entity.TVehicleEntity;
 import org.apache.ibatis.annotations.Mapper;
@@ -7,10 +8,15 @@ import org.apache.ibatis.annotations.Mapper;
 /**
 * 通用车辆管理表
 *
-* @author zhaohong 
+* @author zhaohong
 * @since 1.0.0 2024-03-15
 */
 @Mapper
 public interface TVehicleDao extends BaseDao<TVehicleEntity> {
-	
+    default TVehicleEntity getByLicensePlate(String licensePlate){
+        return this.selectOne(new QueryWrapper<TVehicleEntity>()
+                .eq("license_plate", licensePlate)
+                .eq("deleted", 0));
+    }
+
 }
